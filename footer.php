e58879de325edf294a73b92fdf436318dc55754e
cg99 <darkinfinitespace@gmail.com>
2021-06-19 13:43:17 +0545

    footer

--- footer.php
+++ footer.php
@@ -1,42 +1,46 @@
 <footer class="footer">
-    <?php if( is_active_sidebar( 'spark_blog-footer-left' ) ||
-        is_active_sidebar( 'spark_blog-footer-middle' ) ||
-        is_active_sidebar( 'spark_blog-footer-right' ) ) : ?>
-        <div class="u-full-width">
-          <div class="container ct-footer-widgets">
-            <div class="row">
-              <div class="col-md-4">
-                    <?php
-                            if( is_active_sidebar( 'spark_blog-footer-left' ) ) {
-                                dynamic_sidebar( 'spark_blog-footer-left' );
-                            }
-                        ?>
-              </div><!-- .col-md-4 -->
-              <div class="col-md-4">
-                    <?php
-                        if( is_active_sidebar( 'spark_blog-footer-middle' ) ) {
-                            dynamic_sidebar( 'spark_blog-footer-middle' );
-                        }
-                    ?>
-              </div><!-- .col-md-4 -->
-              <div class="col-md-4">
-                    <?php
-                        if( is_active_sidebar( 'spark_blog-footer-middle' ) ) {
-                            dynamic_sidebar( 'spark_blog-footer-right' );
-                        }
-                    ?>
-              </div><!-- .col-md-4 -->
-            </div>
-          </div><!-- .container -->
+  <?php if (
+    is_active_sidebar('spark_blog-footer-left') ||
+    is_active_sidebar('spark_blog-footer-middle') ||
+    is_active_sidebar('spark_blog-footer-right')
+  ) : ?>
+    <div class="u-full-width">
+      <div class="container ct-footer-widgets">
+        <div class="row">
+          <div class="col-md-4">
+            <?php
+            if (is_active_sidebar('spark_blog-footer-left')) {
+              dynamic_sidebar('spark_blog-footer-left');
+            }
+            ?>
+          </div><!-- .col-md-4 -->
+          <div class="col-md-4">
+            <?php
+            if (is_active_sidebar('spark_blog-footer-middle')) {
+              dynamic_sidebar('spark_blog-footer-middle');
+            }
+            ?>
+          </div><!-- .col-md-4 -->
+          <div class="col-md-4">
+            <?php
+            if (is_active_sidebar('spark_blog-footer-middle')) {
+              dynamic_sidebar('spark_blog-footer-right');
+            }
+            ?>
+          </div><!-- .col-md-4 -->
         </div>
-    <?php endif; ?>
-    <div class="ct-credits">
-      <p><?php esc_html_e( 'Copyright', 'spark-news' ); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a><?php esc_html_e( '. All rights reserved.', 'spark-news' ); ?>
-      <span class="footer-info-right">
-        <?php echo esc_html__(' | Designed by', 'spark-news') ?> <a href="<?php echo esc_url('https://umeshghartimagar.com.np/', 'spark-news'); ?>"><?php echo esc_html__('Umesh GM', 'spark-news') ?></a>
-      </span></p>
+      </div><!-- .container -->
     </div>
+  <?php endif; ?>
+  <div class="ct-credits">
+    <p><?php esc_html_e('Copyright', 'spark-news'); ?> <a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a><?php esc_html_e('. All rights reserved.', 'spark-news'); ?>
+      <span class="footer-info-right">
+        <?php echo esc_html__(' | Designed by', 'spark-news') ?> <a href="<?php echo esc_url('https://umeshghartimagar.com.np/', 'spark-news'); ?>"><?php echo esc_html__('Aim Tech', 'spark-news') ?></a>
+      </span>
+    </p>
+  </div>
 </footer>
 <?php wp_footer(); ?>
-  </body>
+</body>
+
 </html>
